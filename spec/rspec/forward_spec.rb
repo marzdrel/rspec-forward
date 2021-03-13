361536b90105c45091a9f23dcbe4ec6fe35e4505
Marz Drel <marzdrel@users.noreply.github.com>
2021-03-13 21:52:22 +0100

    Add specs for forward_to_instance

--- spec/rspec/forward_spec.rb
+++ spec/rspec/forward_spec.rb
@@ -1,9 +1,73 @@
-RSpec.describe Rspec::Forward do
-  it "has a version number" do
-    expect(Rspec::Forward::VERSION).not_to be nil
+RSpec.describe RSpec::Forward do
+  describe "VERSION" do
+    it "has a version number" do
+      expect(RSpec::Forward::VERSION).not_to be nil
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "with 0-arg class" do
+    before do
+      class TestClass
+        def self.call(...)
+          new(...).call
+        end
+
+        def initialize; end
+        def call; end
+      end
+    end
+
+    after { Object.send(:remove_const, "TestClass") }
+
+    let(:object) { TestClass }
+
+    it "forwards to instance" do
+      expect(object).to forward_to_instance(:call).with_no_args
+    end
+
+    it "fails to forward with arg" do
+      expect { expect(object).to forward_to_instance(:call).with_1_arg }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+
+    it "fails to forward with too many args" do
+      expect { expect(object).to forward_to_instance(:call).with_2_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+  end
+
+  context "with 1-arg class" do
+    before do
+      class TestClass
+        def self.call(...)
+          new(...).call
+        end
+
+        def initialize(arg)
+          @arg = arg
+        end
+
+        def call
+        end
+      end
+    end
+
+    after { Object.send(:remove_const, "TestClass") }
+
+    let(:object) { TestClass }
+
+    it "forwards to instance" do
+      expect(object).to forward_to_instance(:call).with_1_arg
+    end
+
+    it "fails to forward wihout args " do
+      expect { expect(object).to forward_to_instance(:call).with_no_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+
+    it "fails to forward with too many args" do
+      expect { expect(object).to forward_to_instance(:call).with_3_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
   end
 end
