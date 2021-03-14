7c917c18c403dfa92411e0bf2032415040576176
Marz Drel <marzdrel@users.noreply.github.com>
2021-03-14 16:19:14 +0100

    Spearate specs into different files

---
+++ spec/rspec/forward/forward_to_instance_spec.rb
@@ -0,0 +1,93 @@
+RSpec.describe RSpec::Forward::ForwardToInstance do
+  context "with 0-arg class" do
+    before do
+      class TestClass
+        def self.call(...)
+          new(...).call
+        end
+
+        def initialize; end
+        def call; end
+      end
+    end
+
+    after { Object.send(:remove_const, "TestClass") }
+
+    let(:object) { TestClass }
+
+    it "forwards to instance" do
+      expect(object).to forward_to_instance(:call).with_no_args
+    end
+
+    it "fails to forward with arg" do
+      expect { expect(object).to forward_to_instance(:call).with_1_arg }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+
+    it "fails to forward with too many args" do
+      expect { expect(object).to forward_to_instance(:call).with_2_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+  end
+
+  context "with 1-arg class" do
+    before do
+      class TestClass
+        def self.call(...)
+          new(...).call
+        end
+
+        def initialize(arg)
+          @arg = arg
+        end
+
+        def call
+        end
+      end
+    end
+
+    after { Object.send(:remove_const, "TestClass") }
+
+    let(:object) { TestClass }
+
+    it "forwards to instance" do
+      expect(object).to forward_to_instance(:call).with_1_arg
+    end
+
+    it "fails to forward wihout args " do
+      expect { expect(object).to forward_to_instance(:call).with_no_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+
+    it "fails to forward with too many args" do
+      expect { expect(object).to forward_to_instance(:call).with_3_args }
+        .to raise_error RSpec::Mocks::MockExpectationError
+    end
+  end
+
+  context "with named args" do
+    before do
+      class TestClass
+        def self.call(...)
+          new(...).call
+        end
+
+        def initialize(model:)
+          @model = model
+        end
+
+        def call; end
+      end
+    end
+
+    after { Object.send(:remove_const, "TestClass") }
+
+    let(:object) { TestClass }
+
+    it "forwards to instance" do
+      expect(object)
+        .to forward_to_instance(:call)
+        .with_named(model: "name")
+    end
+  end
+end
