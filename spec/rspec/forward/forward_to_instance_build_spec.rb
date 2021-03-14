f177f958efe104d129ee643ed9177c3cfa68bf04
Marz Drel <marzdrel@users.noreply.github.com>
2021-03-14 18:19:35 +0100

    Clean up specs in #forward_to_instance_build

--- spec/rspec/forward/forward_to_instance_build_spec.rb
+++ spec/rspec/forward/forward_to_instance_build_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe RSpec::Forward::ForwardToInstanceBuild do
-  context "with 0-arg class" do
+  context "with no args" do
+    let(:object) { TestClass }
+
     before do
       class TestClass
         def self.call(...)
@@ -13,35 +15,70 @@ RSpec.describe RSpec::Forward::ForwardToInstanceBuild do
       end
     end
 
-    after { Object.send(:remove_const, "TestClass") }
-
-    let(:object) { TestClass }
-
-    it "forwards to instance" do
-      expect(object).to forward_to_instance_build(:call).with_no_args
+    after do
+      Object.send(:remove_const, "TestClass")
     end
 
-    it "fails to forward with arg" do
-      expect { expect(object).to forward_to_instance_build(:call).with_1_arg }
-        .to raise_error RSpec::Mocks::MockExpectationError
+    context "with expected calls" do
+      it "passes" do
+        expect(object).to forward_to_instance_build(:call).with_no_args
+      end
     end
 
-    it "fails to forward with too many args" do
-      expect { expect(object).to forward_to_instance_build(:call).with_2_args }
-        .to raise_error RSpec::Mocks::MockExpectationError
+    context "when expecting 1 argument" do
+      let(:expectation) do
+        proc do
+          expect(object)
+            .to forward_to_instance_build(:call)
+            .with_1_args
+        end
+      end
+
+      it "fails" do
+        expect { expectation.call }
+          .to raise_error(
+            RSpec::Mocks::MockExpectationError,
+          )
+      end
     end
 
-    it "keeps the implementation private" do
-      expect {
-        expect(object)
-          .to forward_to_instance_build(:call)
-          .with_no_args
-          .exp_args
-      }.to raise_error NoMethodError, /private/
+    context "when expecting 2 arguments" do
+      let(:expectation) do
+        proc do
+          expect(object)
+            .to forward_to_instance_build(:call)
+            .with_2_args
+        end
+      end
+
+      it "fails" do
+        expect { expectation.call }
+          .to raise_error(
+            RSpec::Mocks::MockExpectationError,
+          )
+      end
+    end
+
+    context "with calling a private method from mixing" do
+      let(:expectation) do
+        proc do
+          expect(object)
+            .to forward_to_instance_build(:call)
+            .with_no_args
+            .exp_args
+        end
+      end
+
+      it "raises an error" do
+        expect { expectation.call }
+          .to raise_error NoMethodError, /private/
+      end
     end
   end
 
-  context "with 1-arg class" do
+  context "with class accepting one argument" do
+    let(:object) { TestClass }
+
     before do
       class TestClass
         def self.call(...)
@@ -59,26 +96,55 @@ RSpec.describe RSpec::Forward::ForwardToInstanceBuild do
       end
     end
 
-    after { Object.send(:remove_const, "TestClass") }
-
-    let(:object) { TestClass }
-
-    it "forwards to instance" do
-      expect(object).to forward_to_instance_build(:call).with_1_arg
+    after do
+      Object.send(:remove_const, "TestClass")
     end
 
-    it "fails to forward wihout args " do
-      expect { expect(object).to forward_to_instance_build(:call).with_no_args }
-        .to raise_error RSpec::Mocks::MockExpectationError
+    context "with matching expecations" do
+      it "forwards to instance" do
+        expect(object).to forward_to_instance_build(:call).with_1_arg
+      end
     end
 
-    it "fails to forward with too many args" do
-      expect { expect(object).to forward_to_instance_build(:call).with_3_args }
-        .to raise_error RSpec::Mocks::MockExpectationError
+    context "with no args" do
+      let(:expectation) do
+        proc do
+          expect(object)
+            .to forward_to_instance_build(:call)
+            .with_no_args
+        end
+      end
+
+      it "fails" do
+        expect { expectation.call }
+          .to raise_error(
+            RSpec::Mocks::MockExpectationError,
+          )
+      end
+    end
+
+    context "with too many args" do
+      let(:expectation) do
+        proc do
+          expect(object)
+            .to forward_to_instance_build(:call)
+            .with_3_args
+        end
+      end
+
+      it "fails" do
+        expect { expectation.call }
+          .to raise_error(
+            RSpec::Mocks::MockExpectationError,
+            "Wrong number of arguments. Expected 1, got 3.",
+          )
+      end
     end
   end
 
   context "with named args" do
+    let(:object) { TestClass }
+
     before do
       class TestClass
         def self.call(...)
@@ -95,14 +161,16 @@ RSpec.describe RSpec::Forward::ForwardToInstanceBuild do
       end
     end
 
-    after { Object.send(:remove_const, "TestClass") }
+    after do
+      Object.send(:remove_const, "TestClass")
+    end
 
-    let(:object) { TestClass }
-
-    it "forwards to instance" do
-      expect(object)
-        .to forward_to_instance_build(:call)
-        .with_named(:model)
+    context "with expected amount of args" do
+      it "passes" do
+        expect(object)
+          .to forward_to_instance_build(:call)
+          .with_named(:model)
+      end
     end
   end
 end
