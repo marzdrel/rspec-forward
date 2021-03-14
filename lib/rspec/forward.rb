fbb602b0d2d230cc74f0daf9e81e60f6c78bb892
Marz Drel <marzdrel@users.noreply.github.com>
2021-03-14 16:50:50 +0100

    Separate Forward from ForwardBuild

--- lib/rspec/forward.rb
+++ lib/rspec/forward.rb
@@ -1,15 +1,24 @@
 require "rspec"
 require "rspec/mocks"
 require "rspec/forward/version"
+require "rspec/forward/forward_methods"
 require "rspec/forward/forward_to_instance"
+require "rspec/forward/forward_to_instance_build"
 
 module RSpec
   module Forward
     class Error < StandardError; end
+
+    def forward_to_instance(expected)
+      ForwardToInstance.new(expected)
+    end
+
+    def forward_to_instance_build(expected)
+      ForwardToInstanceBuild.new(expected)
+    end
   end
 end
 
 ::RSpec.configure do |config|
-  config.include ::RSpec::Mocks::ExampleMethods
   config.include ::RSpec::Forward
 end
