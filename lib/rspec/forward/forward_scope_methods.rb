286724533613caad8979d21764ec33314ebc986c
Marz Drel <marzdrel@users.noreply.github.com>
2022-08-12 22:37:12 +0200

    Extract "scope" methods to module

---
+++ lib/rspec/forward/forward_scope_methods.rb
@@ -0,0 +1,55 @@
+module ::RSpec
+  module Forward
+    module ForwardScopeMethods
+      using CoreExtensions
+
+      def using_method(target_method_name)
+        @target_method_name = target_method_name
+        self
+      end
+
+      def matches_for?(actual)
+        method_name = @expected
+        base_klass = format("%<method>s_scope", method: method_name.to_s).camelize
+        scope_klass_name = format("%s::%s", actual.to_s, base_klass)
+
+        begin
+          @scope_klass = Object.const_get(scope_klass_name)
+        rescue NameError => e
+          failure_scope_klass_name_not_defined(scope_klass_name)
+          return false
+        end
+
+        allow(@scope_klass)
+          .to receive(@target_method_name)
+          .and_return(:result)
+
+        result = actual.public_send(method_name) == :result
+
+        expect(@scope_klass)
+          .to have_received(@target_method_name)
+
+        result
+      end
+
+      def failure_scope_klass_name_not_defined(name)
+        @failure_message = "#{name} should be defined"
+        @failure_message_when_negated = "#{name} should be defined"
+      end
+
+      def failure_message
+        @failure_message ||
+          %Q(expected "#{@target}.call" to be invoked)
+      end
+
+      def failure_message_when_negated
+        @failure_message_when_negated ||
+          %Q(expected "#{@target}.call" not to be invoked)
+      end
+
+      def self.included(base)
+        private :matches_for?
+      end
+    end
+  end
+end
--- lib/rspec/forward/forward_to_scope.rb
+++ lib/rspec/forward/forward_to_scope.rb
@@ -1,9 +1,8 @@
 module ::RSpec
   module Forward
     class ForwardToScope
-      using CoreExtensions
+      include ForwardScopeMethods
       include Mocks::ExampleMethods
-      include Core::Metadata
 
       def initialize(expected)
         @expected = expected
@@ -13,7 +12,7 @@ module ::RSpec
       end
 
       def matches?(actual)
-        matches_for?(actual, :return)
+        matches_for?(actual)
       end
 
       def description
@@ -21,52 +20,6 @@ module ::RSpec
           to pass the arguments to the constructor of instance and return
           the value returned by the instance method
         TXT
-      end
-
-      def using_method(target_method_name)
-        @target_method_name = target_method_name
-        self
-      end
-
-      private
-
-      def matches_for?(actual, return_value)
-        method_name = @expected
-        base_klass = format("%<method>s_scope", method: method_name.to_s).camelize
-        scope_klass_name = format("%s::%s", actual.to_s, base_klass)
-
-        begin
-          @scope_klass = Object.const_get(scope_klass_name)
-        rescue NameError => e
-          failure_scope_klass_name_not_defined(scope_klass_name)
-          return false
-        end
-
-        allow(@scope_klass)
-          .to receive(@target_method_name)
-          .and_return(:result)
-
-        result = actual.public_send(method_name) == :result
-
-        expect(@scope_klass)
-          .to have_received(@target_method_name)
-
-        result
-      end
-
-      def failure_scope_klass_name_not_defined(name)
-        @failure_message = "#{name} should be defined"
-        @failure_message_when_negated = "#{name} should be defined"
-      end
-
-      def failure_message
-        @failure_message ||
-          %Q(expected "#{@target}.call" to be invoked)
-      end
-
-      def failure_message_when_negated
-        @failure_message_when_negated ||
-          %Q(expected "#{@target}.call" not to be invoked)
       end
     end
   end
