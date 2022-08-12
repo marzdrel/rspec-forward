76b9e2f2e00987e36e5f9982d681b9e163e3039f
Marz Drel <marzdrel@users.noreply.github.com>
2022-08-12 23:41:41 +0200

    Add using_method_args chain spec method

--- lib/rspec/forward/forward_scope_parent_methods.rb
+++ lib/rspec/forward/forward_scope_parent_methods.rb
@@ -13,6 +13,12 @@ module ::RSpec
         self
       end
 
+      def using_method_args(*args, **kwargs)
+        @args = args
+        @kwargs = kwargs
+        self
+      end
+
       def with_parent_arg
         # Name of the parent class is not available here if the default
         # class is used, because defaualt class needs the "actual"
